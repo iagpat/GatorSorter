84e123c4a5b6c0c2debfeb20ef81bb907b5a2233
Iago <iagpat@gmail.com>
2018-06-26 13:51:38 -0400

    This is the first commit. It simply includes the main files.

---
+++ GatorSorter/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: Rent A Center
+ *
+ * Created on June 26, 2018, 1:09 PM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
