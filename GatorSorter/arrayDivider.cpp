1e4eca4c5bffc4967a5691a21bc569188658146a
Iago <iagpat@gmail.com>
2018-06-26 14:54:19 -0400

    added the array divided

---
+++ GatorSorter/arrayDivider.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   arrayDivider.cpp
+ * Author: Rent A Center
+ * 
+ * Created on June 26, 2018, 2:05 PM
+ */
+
+#include "arrayDivider.h"
+
+arrayDivider::arrayDivider() {
+}
+
+arrayDivider::arrayDivider(const arrayDivider& orig) {
+}
+
+arrayDivider::~arrayDivider() {
+}
+
---
+++ GatorSorter/arrayDivider.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   arrayDivider.h
+ * Author: Rent A Center
+ *
+ * Created on June 26, 2018, 2:05 PM
+ */
+
+#ifndef ARRAYDIVIDER_H
+#define ARRAYDIVIDER_H
+
+class arrayDivider {
+public:
+    arrayDivider();
+    arrayDivider(const arrayDivider& orig);
+    virtual ~arrayDivider();
+private:
+
+};
+
+#endif /* ARRAYDIVIDER_H */
+
