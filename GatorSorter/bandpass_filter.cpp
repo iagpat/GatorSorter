59de20f1268dd4faa28b83bf69788d48c21abed2
Iago Patino Lopez <iagpat@gmail.com>
2018-07-09 13:28:29 -0400

    finished bandpass filter

--- GatorSorter/bandpass_filter.cpp
+++ GatorSorter/bandpass_filter.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "filt.h"
 #include <iostream>
+#include <stack>
 #include <fstream>
 
 using namespace std;
@@ -18,7 +19,6 @@ bandpass_filter::bandpass_filter(int64_t * min_freq, int64_t * max_freq, int64_t
 	
 	for (int channel = 0; channel < *number_channels; channel++) { //For every channel 
 		for (int segment = 0; segment < *number_segments; segment++) {//For every segment
-			std::cout << "Filtering channel " << channel << " and segment number " << segment << endl;
 			my_filter = new Filter(BPF, 5, (double)(*sampling_frequency / 1000), (double)(*min_freq / 1000), (double)(*max_freq / 1000));
 
 			//Find the file name of the raw segmented file
@@ -45,28 +45,44 @@ bandpass_filter::bandpass_filter(int64_t * min_freq, int64_t * max_freq, int64_t
 			output = fopen(name, "wb");
 			if (output == 0) std::cout << "Failed to open file" << endl;
 
-			//Debugging
-			std::cout << "The timepoints_per_segment variable is: " << *timepoints_per_segment << endl;
-			fseek(input, 0, SEEK_END);
-			long lSize = ftell(input);
-			rewind(input);
-			std::cout << "The size of the file is: " << lSize << endl;
 
-			//Read and filter file
+			//Declare our stack
+			stack<float> reverse; 
+			stack<float> straight;
+			
+			//Filter data and push to stack
 			for (int timepoint = 0; timepoint < *timepoints_per_segment; timepoint++) { //For every timepoint
 				int problem = fread(&raw, sizeof(float), 1, input);
 				if (problem != 1) {
 					std::cout << "Problem reading the data from the segmented raw file: " << problem << endl;
 				}
 				filtered = my_filter->do_sample(raw);
+				reverse.push(filtered);
+			}
+			free(my_filter);
+			my_filter = new Filter(BPF, 5, (double)(*sampling_frequency / 1000), (double)(*min_freq / 1000), (double)(*max_freq / 1000));
+			//Filter our data in reverse order
+			for (int timepoint = 0; timepoint < *timepoints_per_segment; timepoint++) { //For every timepoint
+				filtered = reverse.top();
+				reverse.pop();
+				filtered = my_filter->do_sample(filtered);
+				straight.push(filtered);
+			}
+			if (reverse.empty() == 0) printf("Problem in the stack system");
+
+			//Write the zero phased data 
+			for (int timepoint = 0; timepoint < *timepoints_per_segment; timepoint++) { //For every timepoint
+				filtered = straight.top();
+				straight.pop();
 				int problem2 = fwrite(&filtered, sizeof(float), 1, output);
 				if (problem2 != 1) printf("Problem wrtiting the filtered data");
 			}
+			if (straight.empty() == 0) printf("Problem in the stack system");
 
 			free(my_filter);
 			fclose(input);
 			fclose(output);
-			std::cout << "Finished filtering channel " << channel << " and segment number " << segment << endl;
+			
 		}
 	}
 	printf("Bandpass filter finished \n");
